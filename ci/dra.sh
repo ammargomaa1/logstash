c5287b7987704500b32ca9b9723a592952645c97
andsel <selva.andre@gmail.com>
2022-09-20 15:50:04 +0200

    Test docker save of the image

--- ci/dra.sh
+++ ci/dra.sh
@@ -20,6 +20,9 @@ echo "Creating dependencies report for ${STACK_VERSION}"
 mkdir -p build/reports/dependencies-reports/
 bin/dependencies-report --csv=build/reports/dependencies-reports/logstash-${STACK_VERSION}.csv
 
+echo "Saving tar.gz for docker images"
+docker save docker.elastic.co/logstash/logstash-full:${STACK_VERSION}-SNAPSHOT | gzip -c > build/logstash-${STACK_VERSION}-SNAPSHOT-docker-image.tar.gz
+
 echo "GENERATED DEPENDENCIES REPORT"
 shasum build/reports/dependencies-reports/logstash-${STACK_VERSION}.csv
 
